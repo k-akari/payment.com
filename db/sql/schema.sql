cefeb9a450020c64680bea4876cd8a394cf3a413
k-akari <tennis.akari.abcdefg@gmail.com>
2023-10-14 12:58:23 +0000

    Add schema.sql

---
+++ db/sql/schema.sql
@@ -0,0 +1,75 @@
+CREATE TABLE `companies` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `representative` varchar(255) NOT NULL,
+  `telephone_number` varchar(255) NOT NULL,
+  `postal_code` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `company_id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`id`),
+  KEY `fk_users_company_id` (`company_id`),
+  CONSTRAINT `fk_users_company_id` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `clients` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `company_id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `representative` varchar(255) NOT NULL,
+  `telephone_number` varchar(255) NOT NULL,
+  `postal_code` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`id`),
+  KEY `fk_clients_company_id` (`company_id`),
+  CONSTRAINT `fk_clients_company_id` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `client_bank_accounts` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `client_id` bigint(20) unsigned NOT NULL,
+  `bank_name` varchar(255) NOT NULL,
+  `branch_name` varchar(255) NOT NULL,
+  `account_number` varchar(255) NOT NULL,
+  `account_name` varchar(255) NOT NULL,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`id`),
+  KEY `fk_client_bank_accounts_client_id` (`client_id`),
+  CONSTRAINT `fk_client_bank_accounts_client_id` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `invoices` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `company_id` bigint(20) unsigned NOT NULL,
+  `client_id` bigint(20) unsigned NOT NULL,
+  `issued_date` datetime(6) NOT NULL,
+  `paid_amount` int(10) unsigned NOT NULL,
+  `fee` int(10) unsigned NOT NULL,
+  `fee_rate` decimal(10,5) unsigned NOT NULL,
+  `sales_tax` int(10) unsigned NOT NULL,
+  `sales_tax_rate` decimal(10,5) unsigned NOT NULL,
+  `billed_amount` int(10) unsigned NOT NULL,
+  `payment_due_date` datetime(6) NOT NULL,
+  `status` tinyint unsigned NOT NULL,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`id`),
+  KEY `fk_invoices_company_id` (`company_id`),
+  CONSTRAINT `fk_invoices_company_id` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`),
+  KEY `fk_invoices_client_id` (`client_id`),
+  CONSTRAINT `fk_invoices_client_id` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
