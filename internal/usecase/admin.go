814c8796c2e99095efceae43da2a52c23359d8ae
k-akari <tennis.akari.abcdefg@gmail.com>
2023-10-14 23:00:40 +0000

    Add usecase for admin

---
+++ internal/usecase/admin.go
@@ -0,0 +1,32 @@
+package usecase
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/k-akari/payment.com/internal/domain"
+)
+
+type adminUsecase struct {
+	companyRepository companyRepository
+}
+
+func NewAdminUsecase(
+	companyRepository companyRepository,
+) *adminUsecase {
+	return &adminUsecase{
+		companyRepository: companyRepository,
+	}
+}
+
+func (u *adminUsecase) CreateCompany(
+	ctx context.Context,
+	company *domain.Company,
+) error {
+	err := u.companyRepository.Create(ctx, company)
+	if err != nil {
+		return fmt.Errorf("failed to run u.companyRepository.Create: %w", err)
+	}
+
+	return nil
+}
---
+++ internal/usecase/interface_repository.go
@@ -0,0 +1,14 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/k-akari/payment.com/internal/domain"
+)
+
+type (
+	companyRepository interface {
+		Create(ctx context.Context, company *domain.Company) error
+		GetByID(ctx context.Context, companyID domain.CompanyID) (*domain.Company, error)
+	}
+)
