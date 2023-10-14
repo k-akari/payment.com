7b1c5df06494db61ee65a87857621f58822c581c
k-akari <tennis.akari.abcdefg@gmail.com>
2023-10-14 14:20:05 +0000

    Add database client

---
+++ internal/infrastructure/database/client.go
@@ -0,0 +1,81 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type (
+	Client interface {
+		Exec(context.Context, string, any) (sql.Result, error)
+		Query(context.Context, string, any) (*sqlx.Rows, error)
+	}
+
+	client struct {
+		db *sqlx.DB
+	}
+)
+
+func NewClient(
+	db *sqlx.DB,
+) Client {
+	return &client{
+		db: db,
+	}
+}
+
+func (c *client) Exec(
+	ctx context.Context,
+	query string,
+	arg any,
+) (sql.Result, error) {
+	namedQuery, namedArgs, err := c.prepare(query, arg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to run c.prepare: %w", err)
+	}
+
+	result, err := c.db.ExecContext(ctx, namedQuery, namedArgs...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to run c.db.ExecContext %w", err)
+	}
+
+	return result, nil
+}
+
+func (c *client) Query(
+	ctx context.Context,
+	query string,
+	arg interface{},
+) (*sqlx.Rows, error) {
+	namedQuery, namedArgs, err := c.prepare(query, arg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to run c.prepare: %w", err)
+	}
+
+	rows, err := c.db.QueryxContext(ctx, namedQuery, namedArgs...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to run c.db.QueryxContext: %w", err)
+	}
+
+	return rows, nil
+}
+
+func (c *client) prepare(
+	query string,
+	arg any,
+) (string, []any, error) {
+	namedQuery, namedArgs, err := c.db.BindNamed(query, arg)
+	if err != nil {
+		return "", nil, fmt.Errorf("failed to run c.db.BindNamed: %w", err)
+	}
+
+	namedQuery, namedArgs, err = sqlx.In(namedQuery, namedArgs...)
+	if err != nil {
+		return "", nil, fmt.Errorf("failed to run sqlx.In: %w", err)
+	}
+
+	return namedQuery, namedArgs, nil
+}
