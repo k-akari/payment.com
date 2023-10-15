d729ad7a5f834f614f0f6b0635d13c77c838b6ef
k-akari <tennis.akari.abcdefg@gmail.com>
2023-10-15 17:27:27 +0000

    Add middleware

--- cmd/api/mux.go
+++ cmd/api/mux.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"net/http"
+	"time"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/k-akari/payment.com/internal/handler"
 	"github.com/k-akari/payment.com/internal/infrastructure/database"
@@ -13,6 +15,13 @@ import (
 
 func newMux(ctx context.Context, cfg *config) (http.Handler, func(), error) {
 	mux := chi.NewRouter()
+
+	mux.Use(middleware.RequestID)
+	mux.Use(middleware.RealIP)
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.Timeout(60 * time.Second))
+
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
