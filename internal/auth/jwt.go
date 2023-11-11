028c9293bb34ca5fb3de0f849172b94b27918273
k-akari <tennis.akari.abcdefg@gmail.com>
2023-11-11 08:01:39 +0000

    Add auth package

---
+++ internal/auth/jwt.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/k-akari/golang-rest-api-sample/internal/domain"
+	"github.com/k-akari/golang-rest-api-sample/internal/pkg/clock"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+)
+
+type JWTer struct {
+	PrivateKey, PublicKey jwk.Key
+	SessionStore          SessionStore
+	Clocker               clock.Clocker
+}
+
+type SessionStore interface {
+	SaveUserID(ctx context.Context, key string, userID domain.UserID) error
+	LoadUserID(ctx context.Context, key string) (domain.UserID, error)
+}
+
+func NewJWTer(rawPrivKey, rawPubKey []byte, s SessionStore) (*JWTer, error) {
+	j := &JWTer{SessionStore: s, Clocker: clock.RealClocker{}}
+
+	privkey, err := parse(rawPrivKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed in NewJWTer: private key: %w", err)
+	}
+	j.PrivateKey = privkey
+
+	pubkey, err := parse(rawPubKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed in NewJWTer: public key: %w", err)
+	}
+	j.PublicKey = pubkey
+
+	return j, nil
+}
+
+func parse(rawKey []byte) (jwk.Key, error) {
+	key, err := jwk.ParseKey(rawKey, jwk.WithPEM(true))
+	if err != nil {
+		return nil, err
+	}
+
+	return key, nil
+}
